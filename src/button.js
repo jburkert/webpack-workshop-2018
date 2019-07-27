6dc5eb772f17645a0f91063c34a9c190ea38389d
Jens Burkert <j.burkert@k-newmedia.de>
2019-07-27 20:43:26 +0200

    031 all module tyoes

---
+++ src/button.js
@@ -0,0 +1,11 @@
+// take a str, the button label and retturn the elenemt
+/**
+ *
+ * @param {string} buttonName
+ * @returns {Element}
+ */
+const makeButton = buttonName => {
+  return `Button: ${buttonName}`
+}
+
+module.exports = makeButton
